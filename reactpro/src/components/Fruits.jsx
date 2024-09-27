edba07a8f7a6f3c2a2900c4792332a83ea3573c3
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2024-09-27 13:38:11 +0500

    Conditionally Rendering A Message Using Ternary

--- reactpro/src/components/Fruits.jsx
+++ reactpro/src/components/Fruits.jsx
@@ -3,17 +3,17 @@ import Fruit from "./Fruit";
 export default function Fruits() {
 //   const fruits = ["Apple", "Mango", "Orange", "Banana"];
 const fruits = [
-    {name: "Apple", price: 10, emoji: "🍎"},
-    {name: "Mango", price: 2, emoji: "🥭"},
-    {name: "Banana", price: 15, emoji: "🍌"},
-    {name: "Pineapple", price: 4, emoji: "🍍"},
-    {name: "Orange", price: 19, emoji: "🍊"},
+    {name: "Apple", price: 10, emoji: "🍎", soldout: false},
+    {name: "Mango", price: 2, emoji: "🥭", soldout: false},
+    {name: "Banana", price: 15, emoji: "🍌", soldout: true},
+    {name: "Pineapple", price: 4, emoji: "🍍", soldout: false},
+    {name: "Orange", price: 19, emoji: "🍊", soldout: true},
 ]
   return (
     <div>
       <ul>
         {fruits.map((fruit) => (
-           <Fruit key={Fruit.name} name={fruit.name} price={fruit.price} emoji={fruit.emoji}/>
+           <Fruit key={Fruit.name} name={fruit.name} price={fruit.price} emoji={fruit.emoji} soldout={fruit.soldout}/>
         ))}
       </ul>
     </div>
