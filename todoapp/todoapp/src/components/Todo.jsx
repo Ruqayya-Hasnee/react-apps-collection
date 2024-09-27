c99cb9dad999b92f010b10ef6c7642eb96ecdc42
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2024-09-27 21:10:45 +0500

    added inline styling

--- todoapp/todoapp/src/components/Todo.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react"
-export default function Todo(){
-    const [todo, setTodo] = useState("")
-    return <div>
-        <input onChange={(e) => setTodo(e.target.value)} value={todo} type="text" />
-        <button type="submit">Add</button>
-    </div>
-}
