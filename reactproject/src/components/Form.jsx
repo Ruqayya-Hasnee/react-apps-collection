5fe657069d87c810d0fe44262a3513f4e03ea30e
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2024-09-27 18:16:26 +0500

    added task of handling multiple inputs in react

--- reactproject/src/components/Form.jsx
+++ reactproject/src/components/Form.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react"
 
 export default function Form(){
-    const [name, setName] = useState("")
-    function handleChange(e){
-        setName(e.target.value);
-    }
-  return <div>
+    const [name, setName] = useState({firstName: "", lastName: ""})
+
+  return (
+  <div>
+     {name.firstName} - {name.lastName}
     <form>
         <input 
-      onChange={(e) => setName(e.target.value)}
+      onChange={(e) => setName({...name,  firstName: e.target.value})}
         type="text" 
-        value={name} />
+        value={name.firstName} 
+        />
+
+        <input type="text" 
+        onChange={(e) => setName({...name, lastName: e.target.value})} 
+        value={name.lastName} />
     </form>
   </div>  
+  )
 }
