f1c06a8473225cd2c451b8ce80f16881b78f6db5
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2024-11-03 14:46:17 +0500

    add a reducer hook

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -3,13 +3,16 @@ import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0);
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, { count: 0, incrementBy: 1 });
   function reducer(state, action) {
     if (action.type == "increment") {
-      return { ...state, count: state.count + action.payload }; // state + action.payload;
+      return { ...state, count: state.count + state.incrementBy }; // state + action.payload;
     }
     if (action.type == "decrement") {
-      return { ...state, count: state.count - action.payload }; // state - action.payload;
+      return { ...state, count: state.count - state.incrementBy }; // state - action.payload;
+    }
+    if (action.type == "setIncrement") {
+      return { ...state, incrementBy: action.payload };
     }
   }
   return (
@@ -18,10 +21,19 @@ function App() {
       {/* <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button> */}
       {state.count}
-      <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
+      <input
+        value={state.incrementBy}
+        onChange={(e) =>
+          dispatch({ type: "setIncrement", payload: Number(e.target.value) })
+        }
+        type="text"
+        name=""
+        id=""
+      />
+      <button onClick={() => dispatch({ type: "increment" })}>
         Increment count
       </button>
-      <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
+      <button onClick={() => dispatch({ type: "decrement" })}>
         Decrement Count
       </button>
     </div>
