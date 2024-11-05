42b99af217bdd360211704627b4748589be0058a
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2024-11-05 15:19:10 +0500

    added new folders for react practice

---
+++ myapp/reactrouterpractice/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Dashboard from "./pages/Dashboard";
+import PageNotFound from "./pages/PageNotFound";
+import Nav from "./pages/Nav";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import Products from "./pages/Products";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/products/:id" element={<Products />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="profile" element={<Profile />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
